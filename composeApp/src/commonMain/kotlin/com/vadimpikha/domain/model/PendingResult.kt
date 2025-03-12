e5a4ef72fa2c44cfbe66115e7f8af9d9c78a88c7
Vadim Pikha <vadimpikha@gmail.com>
2025-03-12 15:46:39 +0200

    minor refactoring

--- composeApp/src/commonMain/kotlin/com/vadimpikha/domain/model/PendingResult.kt
+++ composeApp/src/commonMain/kotlin/com/vadimpikha/domain/model/PendingResult.kt
@@ -3,7 +3,6 @@ package com.vadimpikha.domain.model
 import androidx.compose.runtime.Stable
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.filterIsInstance
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onStart
@@ -29,9 +28,8 @@ fun <T> PendingResult<T>.getOrNull(): T? {
     return null
 }
 
-@Suppress("USELESS_CAST")
 fun <T> Flow<T>.mapToPendingResult(): Flow<PendingResult<T>> {
-    return map { PendingResult.Data(it) as PendingResult<T> }
+    return map<T, PendingResult<T>> { PendingResult.Data(it) }
         .onStart { emit(PendingResult.Loading) }
         .catch { emit(PendingResult.Error(it)) }
 }
